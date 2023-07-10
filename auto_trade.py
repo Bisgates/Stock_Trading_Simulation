b4da7644ba63b7c84ec47bf6e4057b41b5956265
Han Jialu <hanjialu0o0@gmail.com>
2023-07-10 16:52:20 +0800

    feat: support trade multiple stocks

--- auto_trade.py
+++ auto_trade.py
@@ -1,34 +1,49 @@
 import argparse
+import os
 from src.prepare_data import PrepareData
 from src.trader import TradeManager
 
 class AutoTrade:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, stocks_data):
+        self.stocks_data = stocks_data
         self.trade_manager = TradeManager()
-        self.hold_stock = False
+        self.hold_stock = None
 
     def execute(self):
-        for date, row in self.data.iterrows():
-            if not self.hold_stock and row['10MA'] > row['30MA']:
-                self.trade_manager.buy_stock(date, row['Close'])
-                self.hold_stock = True
-            elif self.hold_stock and row['10MA'] < row['30MA']:
-                self.trade_manager.sell_stock(date, row['Close'])
-                self.hold_stock = False
+        for date in sorted(set(date for data in self.stocks_data.values() for date in data.index)):
+            if not self.hold_stock:
+                for stock_name, data in self.stocks_data.items():
+                    if date in data.index and data.loc[date, '10MA'] > data.loc[date, '30MA']:
+                        self.trade_manager.buy_stock(date, data.loc[date, 'Close'], stock_name)
+                        self.hold_stock = stock_name
+                        break
+            elif self.hold_stock is not None and date in self.stocks_data[self.hold_stock].index and self.stocks_data[self.hold_stock].loc[date, '10MA'] < self.stocks_data[self.hold_stock].loc[date, '30MA']:
+                self.trade_manager.sell_stock(date, self.stocks_data[self.hold_stock].loc[date, 'Close'], self.hold_stock)
+                self.hold_stock = None
 
         self.trade_manager.store_transactions()
         self.trade_manager.display_transactions()
         
 
-def main(file_path=None, present_lenth=90):
-    data = PrepareData(file_path).data
-    auto_trade = AutoTrade(data)
+def main(files_path=None):
+    stocks_data = {}
+    if files_path.endswith('.csv'):
+        data = PrepareData(files_path).data
+        stock_name = os.path.splitext(files_path)[0].split('/')[-1]
+        stocks_data[stock_name] = data
+    else:
+        for file_name in os.listdir(files_path):
+            if file_name.endswith('.csv'):
+                file_path = os.path.join(files_path, file_name)
+                data = PrepareData(file_path).data
+                stock_name = os.path.splitext(file_name)[0]
+                stocks_data[stock_name] = data
+    auto_trade = AutoTrade(stocks_data)
     auto_trade.execute()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Stock Trading Visualization and Simulation")
-    parser.add_argument('--file_path', type=str, default='stock_data/apple.csv', help='Path to the CSV file containing stock data')
+    parser.add_argument('--files_path', type=str, default='stock_data/', help='Path to the directory containing CSV files of stock data')
     args = parser.parse_args()
 
-    main(args.file_path)
+    main(args.files_path)
--- src/trader.py
+++ src/trader.py
@@ -12,21 +12,21 @@ class TradeManager:
         self.hold_stock = False
         self.buy_date = None
 
-    def buy_stock(self, current_date, close_price):
+    def buy_stock(self, current_date, close_price, stock_name):
         self.hold_stock = True
         buy_date = current_date.strftime('%Y-%m-%d')
         self.buy_date = datetime.strptime(buy_date, '%Y-%m-%d')  # assuming the date is in 'YYYY-MM-DD' format
-        self.transactions['buy'].append((buy_date, close_price))
-        print(f"Bought stock on {buy_date} at {close_price:.2f}")
+        self.transactions['buy'].append((buy_date, close_price, stock_name))
+        print(f"Bought {stock_name} on {buy_date} at {close_price:.2f}")
 
-    def sell_stock(self, current_date, close_price):
+    def sell_stock(self, current_date, close_price, stock_name):
         sell_date = current_date.strftime('%Y-%m-%d')
         self.sell_date = datetime.strptime(sell_date, '%Y-%m-%d')  # assuming the date is in 'YYYY-MM-DD' format
         hold_duration = (self.sell_date - self.buy_date).days
-        buy_date, buy_price = self.transactions['buy'][-1]
+        buy_price = self.transactions['buy'][-1][1]
         return_rate = self.calculate_return_rate(buy_price, close_price)
         self.transactions['sell'].append((sell_date, close_price, return_rate, hold_duration))
-        print(f"Sold stock on {sell_date} at {close_price:.2f}, Return Rate: {return_rate:.2%}, Hold Duration: {hold_duration} days\n")
+        print(f"Sold {stock_name} on {sell_date} at {close_price:.2f}, Return Rate: {return_rate:.2%}, Hold Duration: {hold_duration} days\n")
         self.hold_stock = False
 
     def calculate_return_rate(self, buy_price, sell_price, with_fee=0.003):
@@ -35,15 +35,40 @@ class TradeManager:
             return_rate *= (1 - with_fee)
         return return_rate
     
+    def store_transactions(self):
+        with open('transactions.csv', 'w', newline='') as csvfile:
+            fieldnames = ['Stock Name', 'Buy Date', 'Sell Date', 'Buy Price', 'Sell Price', 'Return Rate', 'Hold Duration', 'Notes']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            total_return = 1
+            for buy, sell in zip(self.transactions['buy'], self.transactions['sell']):
+                writer.writerow({'Stock Name': buy[2],
+                                 'Buy Date': buy[0], 
+                                 'Sell Date': sell[0], 
+                                 'Buy Price': f"{buy[1]:.2f}", 
+                                 'Sell Price': f"{sell[1]:.2f}", 
+                                 'Return Rate': f"{sell[2]:.2%}", 
+                                 'Hold Duration': sell[3]})
+                total_return *= (1 + sell[2])  # sell tuple: (date, price, return_rate, hold_duration)
+            total_return -= 1
+            
+            first_buy_price = self.transactions['buy'][0][1]
+            last_sell_price = self.transactions['sell'][-1][1]
+            buy_hold_return_rate = self.calculate_return_rate(first_buy_price, last_sell_price)
+
+            writer.writerow({'Notes': f"Total Return: {total_return:.2%}, Buy and Hold Return Rate: {buy_hold_return_rate:.2%}"})
+    
     def display_transactions(self):
         transactions_df = pd.read_csv('transactions.csv')
 
         without_last_row_df = transactions_df.iloc[:-1]
-        selected_columns = ['Buy Date', 'Sell Date', 'Buy Price', 'Sell Price', 'Return Rate', 'Hold Duration']
+        selected_columns = ['Stock Name', 'Buy Date', 'Sell Date', 'Buy Price', 'Sell Price', 'Return Rate', 'Hold Duration']
         without_last_row_df = without_last_row_df[selected_columns]
 
         console = Console()
         table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Stock Name", justify="right")
         table.add_column("Buy Date", justify="right")
         table.add_column("Sell Date", justify="right")
         table.add_column("Buy Price", justify="right")
@@ -62,6 +87,7 @@ class TradeManager:
             else:
                 style = color
             table.add_row(
+                row['Stock Name'], 
                 row['Buy Date'], 
                 row['Sell Date'], 
                 str(row['Buy Price']), 
@@ -74,23 +100,3 @@ class TradeManager:
 
         last_row_note = transactions_df.iloc[-1]['Notes']
         console.print(last_row_note)
-
-    def store_transactions(self):
-        with open('transactions.csv', 'w', newline='') as csvfile:
-            fieldnames = ['Buy Date', 'Sell Date', 'Buy Price', 'Sell Price', 'Return Rate', 'Hold Duration', 'Notes']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-
-            total_return = 1
-            for buy, sell in zip(self.transactions['buy'], self.transactions['sell']):
-                writer.writerow({'Buy Date': buy[0], 'Sell Date': sell[0], 'Buy Price': f"{buy[1]:.2f}", 'Sell Price': f"{sell[1]:.2f}", 'Return Rate': f"{sell[2]:.2%}", 'Hold Duration': sell[3]})
-                total_return *= (1 + sell[2])  # sell tuple: (date, price, return_rate, hold_duration)
-            total_return -= 1
-            
-            first_buy_price = self.transactions['buy'][0][1]
-            last_sell_price = self.transactions['sell'][-1][1]
-            buy_hold_return_rate = self.calculate_return_rate(first_buy_price, last_sell_price)
-
-            writer.writerow({'Notes': f"Total Return: {total_return:.2%}, Buy and Hold Return Rate: {buy_hold_return_rate:.2%}"})
-
-        # self.display_transactions()
